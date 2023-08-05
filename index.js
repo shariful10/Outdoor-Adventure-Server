2365664410a9ac970c2637d8fe1305617f4be9c2
Pranto sheikh <prantosheikh834@gmail.com>
2023-08-06 02:58:01 +0600

    user and message data save mongodb

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bq2ef3t.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bq2ef3t.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,11 +27,40 @@ const client = new MongoClient(uri, {
 
 async function run() {
 	try {
-        const usersCollection = client.db("autdoorDb").collection("users");
-        
+		const usersCollection = client.db("autdoorDb").collection("users");
+		const messageCollection = client.db("autdoorDb").collection("message");
+
+		// post outdoor-adventure user message data
+		app.post("/user", async (req, res) => {
+			const user = req.body;
+			const query = { email: user.email };
+
+			const existingUser = await usersCollection.findOne(query);
+
+			if (existingUser) {
+				return res.send({ message: " user already exists" });
+			}
+			console.log(user);
+			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+
+		app.post("/messageUser", async (req, res) => {
+			const messageData = req.body;
+			console.log(messageData);
+			const result = await messageCollection.insertOne(messageData);
+			res.send(result);
+		});
+
+		// app.get("messageUser", async(req, res) => {
+
+		// });
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
-		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
 	} finally {
 		// Ensures that the client will close when you finish/error
 		// await client.close();
