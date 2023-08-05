5541e787e838342e006e1442a49333dfc2de575d
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-06 01:40:48 +0600

    test 3

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ async function run() {
 		await client.db("admin").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
 	} finally {
-		// Ensures that the client will close when you finish/error
 		// await client.close();
 	}
 }
